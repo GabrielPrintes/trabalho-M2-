7fd64c022168acca7e6f53ebaec660ddc9ab4a07
gabriel <gpprintes@live.com>
2017-10-30 22:09:13 -0200

    inicial

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+
+	cout << "hello world" << endl;
+	system("pause");
+    return 0;
+}
+
